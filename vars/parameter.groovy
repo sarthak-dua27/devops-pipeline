9e0f51e561bcba7175064cf6a315f5435ce658db
Sarthak Dua <110669522+sarthak-dua27@users.noreply.github.com>
2022-12-13 10:19:32 +0530

    Update parameter.groovy

--- vars/parameter.groovy
+++ vars/parameter.groovy
@@ -1,3 +1,7 @@
-def call(String name, String dow){
-  sh "echo ${name} ____ ${dow}"  
+// def call(String name, String dow){
+//   sh "echo ${name} ____ ${dow}"  
+// }
+
+def call(Map config = [:]){
+  sh "echo ${config.name} ____ ${config.dow}"  
 }
