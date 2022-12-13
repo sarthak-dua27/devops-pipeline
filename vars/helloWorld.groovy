0d71d174873056b36e7388cc27ebf2678a0a5f73
Sarthak Dua <110669522+sarthak-dua27@users.noreply.github.com>
2022-12-13 10:27:40 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,11 @@
-def call(){
-  sh "echo Hello World"
+// def call(){
+//   sh "echo Hello World"
+// }
+
+// def call(String name, String dow){
+//   sh "echo ${name} ____ ${dow}"  
+// }
+
+def call(Map config = [:]){
+  sh "echo ${config.name} ____ ${config.dow}"  
 }
